55205ff39177788537ab6d351fc5e75f169240bb
sumitpotdar <sumitseems@gmail.com>
2020-09-26 18:52:17 +0530

    Add files via upload

---
+++ set_services.ps1
@@ -0,0 +1,13 @@
+﻿$ssim = Import-Csv C:\Temp\services.csv
+
+foreach($srs in $ssim){
+
+$srsmm = $srs.Name
+$srssty = $srs.StartMode
+$srssta = $srs.State
+
+Set-Service -Name $srsmm -StartupType $srssty -Status $srssta
+
+Get-Service -Name $srsmm -StartupType $srssty -Status $srssta
+
+}
